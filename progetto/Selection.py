ed05a50df90d5c0d53c30a8c947dfffb3501e971
LucaSantopadre <luca.santopadre92@gmail.com>
2018-11-29 20:17:17 +0100

    progetto mediano dizionario

--- progetto/Selection.py
+++ progetto/Selection.py
@@ -1,4 +1,5 @@
 import random
+import math
 import progetto.Sorting as Sorting
 
 
@@ -9,38 +10,53 @@ def sampleMedianSelect(A):
 
     # scelta random del sottoinsieme V di m elementi
     numGroups = len(A)//m
+    print("numero gruppi:",numGroups)
     randIndex = random.randint(0,numGroups-1)
-    print("indice sottoinsieme",randIndex)
+    print("indice random",randIndex)
 
     B={}
     index=0
     for i in range(randIndex*m,(randIndex*m)+m):
-        print(A[i])
         B[index]=i,A[i]
         index+=1
     print(B)
 
-    #medianDict(B,m)
+    return medianDict(B,m)
     print(B)
-    print(sorted(B.values()))
 
 
 
-# procedura che restituisce la chiave del mediano
-# dato in input un dizionario chiave:elemento
+# procedura che restituisce la CHIAVE del mediano(calcolato per elemento)
+# dato in input un dizionario      chiave:(CHIAVE,elemento)
+# ed "ordinando" tramite algoritmo insertionSort
 def medianDict(dict,m):
+    print("--------------- ORDINAMENTO TRAMITE INSERIONSORT . . .")
     for key in dict:
-        print("chiave:",key,"valore:",dict[key],"DA ORDINARE:",dict[key][1])
+        #print("chiave:",key,"valore:",dict[key],"DA ORDINARE:",dict[key][1])
+        for k in range(1, m):
+            valDict = dict[k]
 
-    print("posizione MEDIANOOOOO",dict[m//2][0])
+            for pos in range(k):
+                if dict[pos][1] > valDict[1]:
+                    #print("dict[pos][1]>val",dict[pos][1],val)
+                    break
+            else:
+                pos = k  # This value for pos is just a flag to remember that from 0 to k, the list is already ordered
+
+            if pos < k:  # pos==k if no pos<k exists s.t. dict[pos][1]>val. Do nothing, and continue from k+1.
+                for j in range(k, pos, -1):
+                    dict[j] = dict[j - 1]
+                dict[pos] = valDict
+    print("FINE --------------- ORDINATO:",dict)
+    print("MEDIANO  posizione:",dict[math.ceil((m-1)//2)][0]," valore:",dict[math.ceil((m-1)//2)][1])
+    return dict[math.ceil((m-1)//2)][0]
 
 
 
 l=[1,20,3,4,5,6,70,8,9,10,110,12,13,14,15]
 
 a=sampleMedianSelect(l)
-
-
+print(a)
 
 
 
