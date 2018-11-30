ab0433f8866c0db3369ed5090705f71e60cf0e87
LucaSantopadre <luca.santopadre92@gmail.com>
2018-11-30 16:20:56 +0100

    progetto 1 bozza ok

--- progetto/Selection.py
+++ progetto/Selection.py
@@ -5,35 +5,50 @@ import progetto.Sorting as Sorting
 
 # Sceglie il pivot su cui partizionare
 # !!! DEVE RITORNARE LA POSIZIONE DEL MEDIANO DEL SOTTOINSIEME V
-def sampleMedianSelect(A):
+def sampleMedianSelect(A,left,right):
     m=5
 
-    # scelta random del sottoinsieme V di m elementi
-    numGroups = len(A)//m
-    print("numero gruppi:",numGroups)
-    randIndex = random.randint(0,numGroups-1)
-    print("indice random",randIndex)
+    # utilizzo lenA per generare il random
+    lenA = right-left+1
+    print(lenA)
 
-    B={}
+    # scelta random del sottoinsieme V di m elementi
+    if(lenA % m == 0):
+        numGroups = lenA//m
+    else:
+        numGroups = lenA//m + 1
+    print("........numero gruppi:",numGroups)
+    randIndex = random.randint(0,numGroups-1)
+    print("........indice random",randIndex)
+    """
+    # select last index of dimension <=m
+    lastIndex = (randIndex*m)+m - 1
+    while(lastIndex >= lenA):
+        lastIndex -= 1
+    """
+    B = {}
     index=0
-    for i in range(randIndex*m,(randIndex*m)+m):
+    for i in range(left,right + 1):
         B[index]=i,A[i]
         index+=1
-    print(B)
+    #print(B)
 
-    return medianDict(B,m)
-    print(B)
+    print("mediano da calcolare sU: ",l[randIndex*m:right + 1])
+    dimension = right - randIndex*m + 1
+    return medianDict(B,m,lenA)
+    #print(B)
 
 
 
 # procedura che restituisce la CHIAVE del mediano(calcolato per elemento)
 # dato in input un dizionario      chiave:(CHIAVE,elemento)
 # ed "ordinando" tramite algoritmo insertionSort
-def medianDict(dict,m):
-    print("--------------- ORDINAMENTO TRAMITE INSERIONSORT . . .")
+def medianDict(dict,m,dimension):
+
+    #print("--------------- ORDINAMENTO TRAMITE INSERIONSORT . . .")
     for key in dict:
         #print("chiave:",key,"valore:",dict[key],"DA ORDINARE:",dict[key][1])
-        for k in range(1, m):
+        for k in range(1, dimension):
             valDict = dict[k]
 
             for pos in range(k):
@@ -47,53 +62,47 @@ def medianDict(dict,m):
                 for j in range(k, pos, -1):
                     dict[j] = dict[j - 1]
                 dict[pos] = valDict
-    print("FINE --------------- ORDINATO:",dict)
-    print("MEDIANO  posizione:",dict[math.ceil((m-1)//2)][0]," valore:",dict[math.ceil((m-1)//2)][1])
-    return dict[math.ceil((m-1)//2)][0]
+    #print("FINE --------------- ORDINATO:",dict)
+    #print("MEDIANO  posizione:",dict[math.ceil((m-1)//2)][0]," valore:",dict[math.ceil((m-1)//2)][1])
+    return dict[math.ceil((dimension-1)//2)][0]
 
 
 
-l=[1,20,3,4,5,6,70,8,9,10,110,12,13,14,15]
 
-a=sampleMedianSelect(l)
-print(a)
 
 
 
 # QuickSort - RECURSIVE, deterministic and non-deterministic
 
-def quickSort(l, det=False):
-    recursiveQuickSort(l, 0, len(l) - 1, det)
+def quickSort(l):
+    recursiveQuickSort(l, 0, len(l) - 1)
 
 
-def recursiveQuickSort(l, left, right, det=False):
-    if printSwitch.dumpOperations:
-        print("recursiveQuickSort({},{})".format(left, right))
-
+def recursiveQuickSort(l, left, right):
     if left >= right:
         return
+    print("LISTA QUICKSORT",l[left:right+1])
+    mid = partition(l, left, right)
+    print("mediano",l[mid],"pos",mid)
+    print("DOPO PARTITION ",l)
+    print("----------------------------------------------------------------")
+    recursiveQuickSort(l, left, mid - 1)
+    print("DESTRAAAA\n")
+    recursiveQuickSort(l, mid + 1, right)
 
-    mid = partition(l, left, right, det)
-    recursiveQuickSort(l, left, mid - 1, det)
-    recursiveQuickSort(l, mid + 1, right, det)
 
-    if printSwitch.dumpOperations:
-        print("- " * left + str(l[left:right + 1]) + " -" * (len(l) - right - 1))
-
-
-def partition(l, left, right, det=False):
+def partition(l, left, right):
     inf = left
     sup = right + 1
 
-    if not det:
-        random.seed(1)
-        mid = random.randint(left, right)
-        l[left], l[mid] = l[mid], l[left]  # exchange first elem with the randomically chosen one
+
+
+
+    mid = sampleMedianSelect(l,left,right)
+    l[left], l[mid] = l[mid], l[left]  # exchange first elem with the randomically chosen one
+
 
     mid = left # the median is the first elem of the array
-
-    if printSwitch.dumpOperations:
-        print("Selected median:", l[mid])
 
     while True:
         inf += 1
@@ -110,3 +119,12 @@ def partition(l, left, right, det=False):
             break
 
     l[mid], l[sup] = l[sup], l[mid]
+
+    return sup
+
+
+
+l=[1,20,3,4,5,6,70,8,9,10,110,12,13,14,3,213,541,52,5,3,643324,1234321,6,454,42,13,214231423,5,6,7,8,1]
+
+quickSort(l)
+print(l)
